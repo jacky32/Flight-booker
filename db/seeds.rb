3d4fb528d2cb405b434015971a788d5df72ddddd
jacky32 <patrki@atlas.cz>
2022-10-07 15:31:11 +0200

    Add airport scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airports = [%w[Prague PRG],
+            %w[Birmingham BHX],
+            %w[Düsseldorf DUS],
+            %w[Rome FCO],
+            %w[Athenas ATH],
+            %w[Istanbul SAW],
+            %w[Vienna VIE],
+            %w[Brussels BRU],
+            %w[London LTN]]
+
+airports.each do |airport|
+  Airport.create(name: airport[0], code: airport[1])
+end
