c855490330929a9dafb7ec00853d499b0db3b88a
jacky32 <patrki@atlas.cz>
2022-10-07 17:00:17 +0200

    Fix flight controller, views

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,5 @@
 class FlightsController < ApplicationController
-  before_action :set_flight, only: %i[ show edit update destroy ]
+  before_action :set_flight, only: %i[show edit update destroy]
 
   # GET /flights or /flights.json
   def index
@@ -7,64 +7,56 @@ class FlightsController < ApplicationController
   end
 
   # GET /flights/1 or /flights/1.json
-  def show
-  end
+  def show; end
 
   # GET /flights/new
   def new
+    @airport_options = Airport.all.map { |airport| [airport.name, airport.id] }
     @flight = Flight.new
   end
 
   # GET /flights/1/edit
   def edit
+    @airport_options = Airport.all.map { |airport| [airport.name, airport.id] }
   end
 
   # POST /flights or /flights.json
   def create
+    @airport_options = Airport.all.map { |airport| [airport.name, airport.id] }
     @flight = Flight.new(flight_params)
 
-    respond_to do |format|
-      if @flight.save
-        format.html { redirect_to flight_url(@flight), notice: "Flight was successfully created." }
-        format.json { render :show, status: :created, location: @flight }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @flight.errors, status: :unprocessable_entity }
-      end
+    if @flight.save
+      redirect_to flight_url(@flight), notice: 'Flight was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /flights/1 or /flights/1.json
   def update
-    respond_to do |format|
-      if @flight.update(flight_params)
-        format.html { redirect_to flight_url(@flight), notice: "Flight was successfully updated." }
-        format.json { render :show, status: :ok, location: @flight }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @flight.errors, status: :unprocessable_entity }
-      end
+    @airport_options = Airport.all.map { |airport| [airport.name, airport.id] }
+    if @flight.update(flight_params)
+      redirect_to flight_url(@flight), notice: 'Flight was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /flights/1 or /flights/1.json
   def destroy
     @flight.destroy
-
-    respond_to do |format|
-      format.html { redirect_to flights_url, notice: "Flight was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to flights_url, notice: 'Flight was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_flight
-      @flight = Flight.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def flight_params
-      params.require(:flight).permit(:arrival_airport_id, :departing_airport_id, :departure, :flight_duration)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_flight
+    @flight = Flight.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def flight_params
+    params.require(:flight).permit(:arrival_airport_id, :departure_airport_id, :departure_datetime, :arrival_datetime)
+  end
 end
--- app/views/flights/_flight.json.jbuilder
+++ app/views/flights/_flight.json.jbuilder
@@ -1,2 +1,3 @@
-json.extract! flight, :id, :arrival_airport_id, :departing_airport_id, :departure, :flight_duration, :created_at, :updated_at
+json.extract! flight, :id, :arrival_airport_id, :departure_airport_id, :departure_datetime, :arrival_datetime,
+              :created_at, :updated_at
 json.url flight_url(flight, format: :json)
