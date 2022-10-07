b661a0dfbf8d2793473e716026cf2c2be61ba51d
jacky32 <patrki@atlas.cz>
2022-10-07 17:11:06 +0200

    Clean airport scaffold

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,5 +1,5 @@
 class AirportsController < ApplicationController
-  before_action :set_airport, only: %i[ show edit update destroy ]
+  before_action :set_airport, only: %i[show edit update destroy]
 
   # GET /airports or /airports.json
   def index
@@ -7,8 +7,7 @@ class AirportsController < ApplicationController
   end
 
   # GET /airports/1 or /airports/1.json
-  def show
-  end
+  def show; end
 
   # GET /airports/new
   def new
@@ -16,34 +15,25 @@ class AirportsController < ApplicationController
   end
 
   # GET /airports/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /airports or /airports.json
   def create
     @airport = Airport.new(airport_params)
 
-    respond_to do |format|
-      if @airport.save
-        format.html { redirect_to airport_url(@airport), notice: "Airport was successfully created." }
-        format.json { render :show, status: :created, location: @airport }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @airport.errors, status: :unprocessable_entity }
-      end
+    if @airport.save
+      redirect_to airport_url(@airport), notice: 'Airport was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /airports/1 or /airports/1.json
   def update
-    respond_to do |format|
-      if @airport.update(airport_params)
-        format.html { redirect_to airport_url(@airport), notice: "Airport was successfully updated." }
-        format.json { render :show, status: :ok, location: @airport }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @airport.errors, status: :unprocessable_entity }
-      end
+    if @airport.update(airport_params)
+      redirect_to airport_url(@airport), notice: 'Airport was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -51,20 +41,18 @@ class AirportsController < ApplicationController
   def destroy
     @airport.destroy
 
-    respond_to do |format|
-      format.html { redirect_to airports_url, notice: "Airport was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to airports_url, notice: 'Airport was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_airport
-      @airport = Airport.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def airport_params
-      params.require(:airport).permit(:name, :code)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_airport
+    @airport = Airport.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def airport_params
+    params.require(:airport).permit(:name, :code)
+  end
 end
--- app/views/flights/_flight.json.jbuilder
+++
@@ -1,3 +0,0 @@
-json.extract! flight, :id, :arrival_airport_id, :departure_airport_id, :departure_datetime, :arrival_datetime,
-              :created_at, :updated_at
-json.url flight_url(flight, format: :json)
