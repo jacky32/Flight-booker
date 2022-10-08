8800e605817639b83fa47143cf95e61500a5425b
jacky32 <patrki@atlas.cz>
2022-10-08 23:48:19 +0200

    Add associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: 'Airport'
   belongs_to :departure_airport, class_name: 'Airport'
+  has_many :bookings
 
   def self.search(departure, arrival, departure_year, departure_month)
     return if departure_month.nil?
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,6 @@ class Passenger < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings
 end
