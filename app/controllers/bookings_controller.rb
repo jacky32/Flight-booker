4b378fde2d49148d897280cd344d8815cc26bab0
jacky32 <patrki@atlas.cz>
2022-10-09 14:39:41 +0200

    Liberate forms, add new booking view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,8 @@
 class BookingsController < ApplicationController
+  def new
+    @flights = Flight.find(params[:flight_id])
+    @passengers = params[:passengers]
+  end
+
+  def create; end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def resource_name
+    :passenger
+  end
+
+  def resource
+    @resource ||= Passenger.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:passenger]
+  end
 end
